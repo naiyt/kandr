2a0df9ecf98bbb3e7a0d1ef2659a8d9cdb547ef7
Nate Collings <nate@natecollings.com>
2013-08-24 17:00:10 -0600

    Minor change

---
+++ chapter4/4-14.c
@@ -0,0 +1,13 @@
+/* Excercise 4-14
+
+   Define a macro swap(t,x,y) that interchanges two arguments of type t. (Block structure will help.) */
+
+#define swap
+
+
+
+int main() {
+
+
+    return 0;
+}
